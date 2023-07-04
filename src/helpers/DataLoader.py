f7a7f6cb4658ebf21c37318e50b1f121de90bf14
AngelaSeleneGarcia <al212146@alumnos.uacj.mx>
2023-07-04 15:22:07 -0600

    Poo examples

--- src/helpers/DataLoader.py
+++ src/helpers/DataLoader.py
@@ -5,6 +5,6 @@ class Dataloader:
     def __init__(self, path):
         self.path = path
 
-    def load_data(path):
+    def load_data(self):
         data = pd.read_csv(self.path)
         return data
